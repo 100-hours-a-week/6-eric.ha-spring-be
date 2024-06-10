079a8e86f2c1ca148d32d0b3d608406f50714a04
하남규 <hanamgyu@hanamgyuui-MacBookPro.local>
2024-06-10 15:55:29 +0900

    Initial commit

---
+++ src/test/java/com/ktb/ktb_cj_community_spring_be/KtbCjCommunitySpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.ktb_cj_community_spring_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbCjCommunitySpringBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
