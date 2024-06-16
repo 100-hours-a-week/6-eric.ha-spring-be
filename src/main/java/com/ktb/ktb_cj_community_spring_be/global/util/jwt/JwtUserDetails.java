89e950647f3421e49f41eaa3998d6d8993e98550
Namgyu11 <hagaji3@naver.com>
2024-06-17 00:53:26 +0900

    feat: JwtUserDetails,JwtUserDetailService 구현
    - 사용자 권한 및 인증 정보 관리 구현
    - 사용자 인증 정보 로드
    - 메소드 최적화 및 구현

---
+++ src/main/java/com/ktb/ktb_cj_community_spring_be/global/util/jwt/JwtUserDetails.java
@@ -0,0 +1,46 @@
+package com.ktb.ktb_cj_community_spring_be.global.util.jwt;
+
+import com.ktb.ktb_cj_community_spring_be.global.util.jwt.dto.CustomUserDto;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record JwtUserDetails(CustomUserDto customUserDto) implements UserDetails {
+
+      @Override
+      public Collection<? extends GrantedAuthority> getAuthorities() {
+            return List.of(new SimpleGrantedAuthority(customUserDto.getRoleType()));
+      }
+
+      @Override
+      public String getPassword() {
+            return customUserDto.getPassword();
+      }
+
+      @Override
+      public String getUsername() {
+            return customUserDto.getEmail();
+      }
+
+      @Override
+      public boolean isAccountNonExpired() {
+            return true;
+      }
+
+      @Override
+      public boolean isAccountNonLocked() {
+            return true;
+      }
+
+      @Override
+      public boolean isCredentialsNonExpired() {
+            return true;
+      }
+
+      @Override
+      public boolean isEnabled() {
+            return true;
+      }
+}
