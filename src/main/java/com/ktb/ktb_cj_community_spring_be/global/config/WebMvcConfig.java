a1d75b0bd9dc603ce9e43ab9fd135d01e3e87896
Namgyu11 <hagaji3@naver.com>
2024-06-20 04:21:05 +0900

    feat: JPA 감사 및 웹 MVC 설정 클래스 추가
    - JPA 감사 기능 설정 클래스 추가
      - JpaAuditingConfig.java: JPA 감사 기능을 활성화하여 엔티티 생성 및 수정 시 자동으로 시간 정보를 기록
    
    - 웹 MVC 설정 클래스 추가
      - WebMvcConfig.java: CORS 설정 등 웹 MVC 관련 설정을 담당하여 웹 애플리케이션의 전반적인 설정을 구성

---
+++ src/main/java/com/ktb/ktb_cj_community_spring_be/global/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.ktb.ktb_cj_community_spring_be.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer{
+
+}
