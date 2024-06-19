c152604b4ecc99794369b72017257067c1026abb
Namgyu11 <hagaji3@naver.com>
2024-06-20 04:17:37 +0900

    refactor: 회원 권한 필드 변수명 변경
    - role -> roleType

--- src/main/java/com/ktb/ktb_cj_community_spring_be/member/entity/Member.java
+++ src/main/java/com/ktb/ktb_cj_community_spring_be/member/entity/Member.java
@@ -3,6 +3,7 @@ package com.ktb.ktb_cj_community_spring_be.member.entity;
 import com.ktb.ktb_cj_community_spring_be.comment.entity.Comment;
 import com.ktb.ktb_cj_community_spring_be.comment.entity.CommentLike;
 import com.ktb.ktb_cj_community_spring_be.global.entity.BaseEntity;
+import com.ktb.ktb_cj_community_spring_be.member.entity.type.MemberRoleType;
 import com.ktb.ktb_cj_community_spring_be.post.entity.Post;
 import com.ktb.ktb_cj_community_spring_be.post.entity.PostLike;
 import jakarta.persistence.*;
@@ -24,39 +25,42 @@ import lombok.Setter;
 @Builder
 public class Member extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+      @Column(nullable = false, unique = true)
+      private String email;
 
-    @Column(nullable = false)
-    private String password;
+      @Column(nullable = false)
+      private String password;
 
-    @Column(nullable = false, unique = true, length = 50)
-    private String nickname;
+      @Column(nullable = false, unique = true, length = 50)
+      private String nickname;
 
-    @Column(nullable = false, length = 50)
-    private String role;
+      @Column(nullable = false, length = 50)
+      private MemberRoleType roleType;
 
-    @Builder.Default
-    private boolean emailAuth = false;
+      @Column(nullable = false)
+      private String profileImageUrl;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Post> posts = new ArrayList<>();
+//    @Builder.Default
+//    private boolean emailAuth = false;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments = new ArrayList<>();
+      @Builder.Default
+      @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+      private List<Post> posts = new ArrayList<>();
 
-    @Builder.Default
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PostLike> postLikes = new ArrayList<>();
+      @Builder.Default
+      @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+      private List<Comment> comments = new ArrayList<>();
 
-    @Builder.Default
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CommentLike> commentLikes = new ArrayList<>();
+      @Builder.Default
+      @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+      private List<PostLike> postLikes = new ArrayList<>();
+
+      @Builder.Default
+      @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+      private List<CommentLike> commentLikes = new ArrayList<>();
 
 }
