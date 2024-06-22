d5ebfeb0024f6fa4629d32fdafd645447a88bea1
Namgyu11 <hagaji3@naver.com>
2024-06-23 03:12:41 +0900

    feat: Post api Base 기반 작업

---
+++ src/main/java/com/ktb/ktb_cj_community_spring_be/post/service/PostService.java
@@ -0,0 +1,23 @@
+package com.ktb.ktb_cj_community_spring_be.post.service;
+
+import com.ktb.ktb_cj_community_spring_be.post.dto.PostRequest;
+import com.ktb.ktb_cj_community_spring_be.post.dto.PostResponse;
+import com.ktb.ktb_cj_community_spring_be.post.entity.Post;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface PostService {
+
+
+      PostResponse createPost(PostRequest request, List<MultipartFile> images, String email);
+
+      PostResponse readPost(Long postId);
+
+      PostResponse updatePost(Long id, PostRequest request, List<MultipartFile> newImages,
+              List<Long> imageIdsToDelete, String email);
+
+      PostResponse deletePost(Long id, String email);
+
+      Post uploadS3Image(PostRequest request, List<MultipartFile> multipartFiles);
+
+}
---
+++ src/main/java/com/ktb/ktb_cj_community_spring_be/post/web/PostController.java
@@ -0,0 +1,9 @@
+package com.ktb.ktb_cj_community_spring_be.post.web;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+}
